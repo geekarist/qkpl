4bb4249b119cc1e20fda95ff66181973d16deea5
Christophe Pelé <97438970+cpele@users.noreply.github.com>
2026-04-10 18:22:09 +0200

    Module system change, HTTP server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Yo");
+import http from "node:http"
+
+const server = http.createServer(async (req, res) => {
+    res.writeHead(200, { "content-type": "text/plain" })
+    res.end("Yo")
+})
+
+const PORT = 8080
+
+server.listen(PORT, () => {
+    console.log("Server started")
+    console.log(`Listening on URL: http://localhost:${PORT}`)
+})
