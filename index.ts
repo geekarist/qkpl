12e47a99ad1af00101991af6b62238a7d88584b2
Christophe Pelé <geekarist@users.noreply.github.com>
2026-04-15 09:24:10 +0200

    Call actual geocoding API

--- index.ts
+++ index.ts
@@ -1,25 +1,23 @@
 import express from "express"
+import secretConfig from './secret/config.json' with { type: "json" }
 
 const app = express()
 
-app.get('/api/places', (req, res) => {
+app.get('/api/places', async (req, res) => {
     const searchText = req.query.q
     res.setHeader('content-type', 'application/json')
+    const remoteToken = secretConfig.jawg["access-token"]
+    const remoteRes = await fetch(
+        `https://api.jawg.io/places/v1/autocomplete?text=${searchText}&access-token=${remoteToken}`
+    )
+    const remoteResJson = await remoteRes.json()
     res.send({
-        "places": [
-            {
-                "name": "Mairie",
-                "city": "Montigny-sur-Loing"
-            },
-            {
-                "name": "Supermarché",
-                "city": "Montigny-sur-Loing"
-            },
-            {
-                "name": "Gymnase",
-                "city": "Montigny-sur-Loing"
-            }
-        ]
+        places: remoteResJson.features.map(
+            (feature: any) => {
+                return {
+                    name: feature.properties.label
+                }
+            })
     })
 })
 
